39357ae58e8e110817d439e2b829128e87da0cc5
Антон Майдаровский <anton-vm@yandex.ru>
2022-12-22 22:36:51 +0300

    Add working version without classes

--- main.py
+++ main.py
@@ -16,16 +16,10 @@ def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(3, 5)}')
-        return (f'{char_name} нанёс урон противнику'
-                f' равный {5 + randint(3, 5)}')
     if char_class == 'mage':
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(5, 10)}')
-        return (f'{char_name} нанёс урон противнику'
-                f' равный {5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику'
-                f' равный {5 + randint(-3, -1)}')
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(-3, -1)}')
     return (f'{char_name} не умеет наносить урон')
@@ -35,12 +29,9 @@ def defence(char_name: str, char_class: str) -> str:
     """Блок тренировки защиты для разных типов персонажей."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
-        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
-        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
     return (f'{char_name} не умеет блокировать')
 
@@ -70,7 +61,6 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
     cmd: str = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
@@ -111,12 +101,10 @@ if __name__ == '__main__':
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
-    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
